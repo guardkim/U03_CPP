0f1dfb32bc0aaae42db034482167334920a901ce
GuardKim <kimkyoungho0726@gmail.com>
2021-11-17 12:45:48 +0900

    20211117_End

---
+++ U03_Game/Source/U03_Game/Characters/CEnemy_AI.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CEnemy.h"
+#include "CEnemy_AI.generated.h"
+
+UCLASS()
+class U03_GAME_API ACEnemy_AI : public ACEnemy
+{
+	GENERATED_BODY()
+	
+};
